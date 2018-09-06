d302c017016766373e882e52f06169355ac1bcfe
havard <havardnoren@gmail.com>
2018-09-06 09:54:28 +0200

    feat: set correct title for page

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         <Navbar fluid>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
+              <a href="#">Red Otter - Registration Form</a>
             </Navbar.Brand>
             <Button
               bsStyle="primary"
